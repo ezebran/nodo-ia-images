222d0ba558b4baaba58b5aee366b5ac0ea183cc8
Ezequiel Brandan <ezebran@gmail.com>
2023-06-13 19:52:08 -0300

    changes

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -13,7 +13,7 @@ const generateImage = async (req, res) => {
     const response = await openai.createImage({
       prompt,
       n: 1,
-      size: "512×512",
+      size: '512x512',
     });
 
     const imageUrl = response.data.data[0].url;
