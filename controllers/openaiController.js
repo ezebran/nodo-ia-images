1648df3eaba7aed354c5622e3255ed0a01074f4e
Ezequiel Brandan <ezebran@gmail.com>
2023-06-13 21:02:26 -0300

    new

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
